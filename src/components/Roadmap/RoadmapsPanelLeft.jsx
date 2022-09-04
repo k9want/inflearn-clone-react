63f65689511f292a72bc47de39f4c36af29f70af
qma <zndwlrnjr26@daum.net>
2022-09-05 00:51:06 +0900

    💄 Implement RoadmapsPanelLeft Component nav-toggle

---
+++ src/components/Roadmap/RoadmapsPanelLeft.jsx
@@ -0,0 +1,148 @@
+import React, { useState } from 'react'
+
+function RoadmapsPanelLeft() {
+  const [open, setOpen] = useState([true, true, true])
+
+  function openList(i) {
+    let openCopy = [...open]
+    openCopy[i] = !open[i]
+    setOpen(openCopy)
+  }
+
+  return (
+    <div className="panel-left sm-rm-hidden">
+      <div className="select-sort">
+        <select name="sortby">
+          <option value="rating">추천순</option>
+          <option value="cnt">학생수순</option>
+          <option value="like">좋아요순</option>
+          <option value="share">공유순</option>
+          <option value="new">최신순</option>
+          <option value="old">오래된순</option>
+        </select>
+      </div>
+
+      <nav>
+        <h1 className="visually-hidden">checkbox navigation</h1>
+
+        <div className="nav-list-group">
+          <div
+            className={open[0] ? 'nav-toggle is-active' : 'nav-toggle'}
+            onClick={() => openList(0)}
+          >
+            분야별
+          </div>
+          {open[0] ? (
+            <div className="nav-list">
+              <div className="nav-checkbox">
+                <input type="checkbox" id="term1" className="visually-hidden" />
+                <label for="term1">개발 · 프로그래밍</label>
+              </div>
+              <div className="nav-checkbox">
+                <input type="checkbox" id="term2" className="visually-hidden" />
+                <label for="term2">보안 · 네트워크</label>
+              </div>
+              <div className="nav-checkbox">
+                <input type="checkbox" id="term3" className="visually-hidden" />
+                <label for="term3">데이터 사이언스</label>
+              </div>
+              <div className="nav-checkbox">
+                <input type="checkbox" id="term4" className="visually-hidden" />
+                <label for="term4">게임 개발</label>
+              </div>
+              <div className="nav-checkbox">
+                <input type="checkbox" id="term5" className="visually-hidden" />
+                <label for="term5">크리에이티브</label>
+              </div>
+              <div className="nav-checkbox">
+                <input type="checkbox" id="term6" className="visually-hidden" />
+                <label for="term6">직무 · 마케팅</label>
+              </div>
+              <div className="nav-checkbox">
+                <input type="checkbox" id="term7" className="visually-hidden" />
+                <label for="term7">학문 · 외국어</label>
+              </div>
+              <div className="nav-checkbox">
+                <input type="checkbox" id="term8" className="visually-hidden" />
+                <label for="term8">커리어</label>
+              </div>
+              <div className="nav-checkbox">
+                <input type="checkbox" id="term9" className="visually-hidden" />
+                <label for="term9">교양</label>
+              </div>
+            </div>
+          ) : null}
+
+          <div
+            className={'nav-toggle ' + (open[1] ? 'is-active' : null)}
+            onClick={() => openList(1)}
+          >
+            시작 레벨
+          </div>
+          {open[1] ? (
+            <div className="nav-list">
+              <div className="nav-checkbox">
+                <input
+                  type="checkbox"
+                  id="level1"
+                  className="visually-hidden"
+                />
+                <label for="level1">입문</label>
+              </div>
+              <div className="nav-checkbox">
+                <input
+                  type="checkbox"
+                  id="level2"
+                  className="visually-hidden"
+                />
+                <label for="level2">초급</label>
+              </div>
+              <div className="nav-checkbox">
+                <input
+                  type="checkbox"
+                  id="level3"
+                  className="visually-hidden"
+                />
+                <label for="level3">중급</label>
+              </div>
+            </div>
+          ) : null}
+
+          <div
+            className={'nav-toggle ' + (open[2] ? 'is-active' : null)}
+            onClick={() => openList(2)}
+          >
+            로드맵
+          </div>
+          {open[2] ? (
+            <div className="nav-list">
+              <div className="nav-checkbox">
+                <input
+                  type="checkbox"
+                  id="enroll1"
+                  className="visually-hidden"
+                />
+                <label for="enroll1">미참여 로드맵</label>
+              </div>
+              <div className="nav-checkbox">
+                <input
+                  type="checkbox"
+                  id="enroll2"
+                  className="visually-hidden"
+                />
+                <label for="enroll2">참여 로드맵</label>
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </nav>
+
+      <a href="/" className="filter-reset">
+        <i className="ic-sync"></i>
+        &nbsp;&nbsp; 필터 초기화
+      </a>
+    </div>
+  )
+}
+
+export default RoadmapsPanelLeft
