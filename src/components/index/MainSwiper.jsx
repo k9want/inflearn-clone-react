24d31485bbbcad148394c99b0116c13ae7fb5931
qma <zndwlrnjr26@daum.net>
2022-09-13 20:03:14 +0900

    ✨ Implement mainBanner swiper

--- src/components/index/MainSwiper.jsx
+++ src/components/index/MainSwiper.jsx
@@ -1,87 +1,91 @@
 import React from 'react'
-import swiperImg from '../../assets/images/main1.gif'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css/bundle'
+import 'swiper/css/pagination'
+import { Navigation, Pagination } from 'swiper'
+import { useState } from 'react'
 
-function MainSwiper() {
+function MainSwiper({ mainBanner }) {
   return (
-    <section class="swiper">
-      <h1 class="visually-hidden">swiper section</h1>
-      <div class="swiper-img">
-        <a href="/">
-          <div class="container">
-            <div class="swiper-img-wrapper">
-              <div class="swiper-img-left">
-                <div class="swiper-img-left-box1">
-                  <div>
-                    <span>함께해요</span>
+    <section className="swiper-section">
+      <h1 className="visually-hidden">swiper section</h1>
+      <Swiper
+        className="swiper"
+        tag="section"
+        slidesPerView={1}
+        onInit={(swiper) => console.log('swiper init')}
+        onSlideChange={(swiper) => console.log('slide change')}
+        onReachEnd={() => console.log('swiper end reached')}
+        loop="true"
+        loopedSlides={SwiperSlide.length}
+      >
+        {mainBanner.map((data, i) => {
+          return (
+            <SwiperSlide
+              key={`slide-${i}`}
+              tag="div"
+              className="swiper-img"
+              style={data.backgroundColor}
+            >
+              <a href="/">
+                <div className="container">
+                  <div className="swiper-img-wrapper">
+                    <div className="swiper-img-left">
+                      <div className="swiper-img-left-box1">
+                        <div style={data.spanColor}>
+                          <span>{data.span}</span>
+                        </div>
+                        <h1 style={data.textColor}>
+                          {data.title1}
+                          <br />
+                          {data.title2}
+                        </h1>
+                        <p style={data.textColor}>{data.description1}</p>
+                        <p style={data.textColor}>{data.description2}</p>
+                      </div>
+                    </div>
+                    <div className="swiper-img-right">
+                      <div className="swiper-img-right-box1">
+                        <img
+                          src={require('../../assets/images/index/' +
+                            data.imgUrl)}
+                          alt="{data.bullet}"
+                        />
+                      </div>
+                    </div>
                   </div>
-                  <h1>
-                    함께 성장할 <br />
-                    동료를 찾아요 🌿
-                  </h1>
-                  <p>우리 #함께성장해요</p>
-                  <p>#동료여어디계신가요?</p>
                 </div>
-              </div>
-              <div class="swiper-img-right">
-                <div class="swiper-img-right-box1">
-                  <img src={swiperImg} alt="인프런과 함께해요" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </a>
-      </div>
-      <div class="swiper-pagination">
-        <div class="container">
-          <div class="swiper-pagination-wrapper">
-            <div class="swiper-pagination-controller">
+              </a>
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
+
+      <div className="swiper-pagination-group">
+        <div className="container">
+          <div className="swiper-pagination-wrapper">
+            <div className="swiper-pagination-controller">
               <span>6/7</span>
               <div>
-                <i class="ic-previous"></i>
-                <i class="ic-pause"></i>
-                <i class="ic-next"></i>
+                <i className="ic-previous"></i>
+                <i className="ic-pause"></i>
+                <i className="ic-next"></i>
               </div>
             </div>
-            <div class="divider"></div>
-            <ul class="swiper-pagination-list">
-              <li>
-                <button class="swiper-pagination-btn" type="button">
-                  Top 50 👑
-                </button>
-              </li>
-              <li>
-                <button class="swiper-pagination-btn" type="button">
-                  입문 로드맵
-                </button>
-              </li>
-              <li>
-                <button class="swiper-pagination-btn" type="button">
-                  신규 강의 🎁
-                </button>
-              </li>
-              <li>
-                <button class="swiper-pagination-btn" type="button">
-                  왕초보 모여라 😎
-                </button>
-              </li>
-              <li>
-                <button class="swiper-pagination-btn" type="button">
-                  지식공유신청
-                </button>
-              </li>
-              <li>
-                <button class="swiper-pagination-btn is-focus" type="button">
-                  인프런 동료찾기
-                </button>
-              </li>
-              <li class="is-focus">
-                <button class="swiper-pagination-btn" type="button">
-                  인프런은 🌱
-                </button>
-              </li>
+            <div className="divider"></div>
+            <ul className="swiper-pagination-list">
+              {mainBanner.map((data, i) => {
+                return (
+                  <li key={`pagination-${i}`}>
+                    <span className="swiper-pagination-btn" type="button">
+                      {data.bullet}
+                    </span>
+                  </li>
+                )
+              })}
             </ul>
-            <button class="swiper-pagination-dropdown" type="button">
-              <i class="ic-down"></i>
+            <button className="swiper-pagination-dropdown" type="button">
+              <i className="ic-down"></i>
             </button>
           </div>
         </div>
