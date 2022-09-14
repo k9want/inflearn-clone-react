1fc62a9a618c2bf8ef0194e50e8777a78f07f80f
qma <zndwlrnjr26@daum.net>
2022-09-14 22:42:16 +0900

    ✨ Implement Company Component

--- src/components/index/Company.jsx
+++ src/components/index/Company.jsx
@@ -1,38 +1,32 @@
 import React from 'react'
 
 function Company() {
+  const companyArr = [
+    '라인',
+    '우아한형제들',
+    'SK',
+    '네이버',
+    '넥슨',
+    '삼성',
+    '카카오',
+    'LG CNS',
+    'NC',
+  ]
   return (
     <section class="company">
       <h3>이미 많은 기업 구성원들이 인프런에서 성장하고 있어요.</h3>
 
       <div class="company-wrapper">
-        <div>
-          <img src="./assets/images/company-1.jpeg" alt="라인" />
-        </div>
-        <div>
-          <img src="./assets/images/company-2.jpeg" alt="우아한형제들" />
-        </div>
-        <div>
-          <img src="./assets/images/company-3.jpeg" alt="SK" />
-        </div>
-        <div>
-          <img src="./assets/images/company-4.jpeg" alt="네이버" />
-        </div>
-        <div>
-          <img src="./assets/images/company-5.jpeg" alt="넥슨" />
-        </div>
-        <div>
-          <img src="./assets/images/company-6.jpeg" alt="삼성" />
-        </div>
-        <div>
-          <img src="./assets/images/company-7.jpeg" alt="카카오" />
-        </div>
-        <div>
-          <img src="./assets/images/company-8.jpeg" alt="LG CNS" />
-        </div>
-        <div>
-          <img src="./assets/images/company-9.jpeg" alt="NC" />
-        </div>
+        {companyArr.map((company, i) => {
+          return (
+            <div>
+              <img
+                src={require(`../../assets/images/index/company-${i + 1}.jpeg`)}
+                alt={company}
+              />
+            </div>
+          )
+        })}
       </div>
     </section>
   )
