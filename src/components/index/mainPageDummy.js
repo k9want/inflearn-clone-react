20d3ea6c90e80e1cf0be7761e8fcd9253f681969
qma <zndwlrnjr26@daum.net>
2022-09-16 23:41:46 +0900

    🍱 Upload banner2 images and add banner2 dummy data

--- src/components/index/mainPageDummy.js
+++ src/components/index/mainPageDummy.js
@@ -613,5 +613,29 @@ export const mainPageDummy = {
         skill: '카피라이팅, 서비스 기획',
       },
     ],
+    banner2: [
+      {
+        imgUrl:
+          'https://cdn.inflearn.com/public/main_sliders/38744ba0-b12a-491a-9ca2-43375c3905ba/share-you.jpg',
+        title1: '지식을 나눠주세요. ',
+        title2: '쉽게 시작하고 합당한 보상을 받을 수 있어요.',
+        subtext: '지식공유 알아보기',
+      },
+      {
+        imgUrl:
+          'https://cdn.inflearn.com/public/main_sliders/3b67a1a1-aae3-442e-8e17-791b7cdfb5f5/bottom-1920-205-%281111%29.jpg',
+        title1: '모든 팀원이 인프런의 강의들을',
+        title2: '자유롭게 학습하는 환경을 제공해주세요.',
+        subtext: '비즈니스 알아보기',
+      },
+      {
+        imgUrl:
+          'https://cdn.inflearn.com/public/main_sliders/39bcb91f-64a4-4a16-8878-e8dc7cd342e8/bottom-1920-205-%28221%29.jpg',
+        fontColor: '#ffffff',
+        title1: '당신은 더 좋은 곳에 갈 수 있어요.',
+        title2: '지금 열려있는 채용공고를 확인해보세요.',
+        subtext: '공고 확인하기',
+      },
+    ],
   },
 }
