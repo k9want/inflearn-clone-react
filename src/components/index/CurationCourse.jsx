d1c5fe4b4f7260d677c27e6ac002ea6080082992
qma <zndwlrnjr26@daum.net>
2022-10-27 00:35:07 +0900

    💄 Implement CurationCourse Component responsive layout

--- src/components/index/CurationCourse.jsx
+++ src/components/index/CurationCourse.jsx
@@ -4,34 +4,37 @@ function CurationCourse({ curationCourse }) {
   return (
     <section className="curation-course">
       <div className="container">
-        <header className="course-header">
-          <a href="/">
-            <h1 className="course-title">
-              읽어보기
-              <i className="ic-next"></i>
-            </h1>
-          </a>
-        </header>
+        <div className="course-list">
+          <header className="course-header">
+            <a href="/">
+              <h1 className="course-title">
+                읽어보기
+                <i className="ic-next"></i>
+              </h1>
+            </a>
+          </header>
 
-        <div className="card-list">
-          {curationCourse.map((data, i) => {
-            return (
-              <div className="curation-card" key={`curationCourse-${i}`}>
-                <a href="/">
-                  <div>
-                    <img
-                      src={require('../../assets/images/index/' + data.imgUrl)}
-                      alt={data.title}
-                    />
+          <div className="card-list">
+            {curationCourse.map((data, i) => {
+              return (
+                <div className="curation-card" key={`curationCourse-${i}`}>
+                  <div className="curation-card-wrapper">
+                    <div className="curation-card-img">
+                      <img
+                        src={require('../../assets/images/index/' +
+                          data.imgUrl)}
+                        alt={data.title}
+                      />
+                    </div>
+                    <div className="card-content">
+                      <h3>{data.title}</h3>
+                      <p>{data.description}</p>
+                    </div>
                   </div>
-                  <div className="card-content">
-                    <h3>{data.title}</h3>
-                    <p>{data.description}</p>
-                  </div>
-                </a>
-              </div>
-            )
-          })}
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
     </section>
